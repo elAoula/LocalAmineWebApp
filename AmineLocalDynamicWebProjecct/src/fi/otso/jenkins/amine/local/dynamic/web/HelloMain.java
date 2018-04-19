bc8b13c05679ab50d5b635497f2df381ddba6354
el.amine.aoula <el.amine.aoula@gmail.com>
2018-04-19 10:48:30 +0300

    Amine: Added Java classes

---
+++ AmineLocalDynamicWebProjecct/src/fi/otso/jenkins/amine/local/dynamic/web/HelloMain.java
@@ -0,0 +1,12 @@
+package fi.otso.jenkins.amine.local.dynamic.web;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		
+		System.out.println(new SayHello("Amine aoula ")); ;
+
+	}
+
+}
---
+++ AmineLocalDynamicWebProjecct/src/fi/otso/jenkins/amine/local/dynamic/web/SayHello.java
@@ -0,0 +1,12 @@
+package fi.otso.jenkins.amine.local.dynamic.web;
+
+public class SayHello {
+
+	String s = "Hello ";
+	public SayHello(String messgae) {
+
+		s = s.concat(messgae);
+	}
+
+
+}
